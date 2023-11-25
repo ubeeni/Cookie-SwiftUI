af57b3f53dd0c924ab992d2a691e88d0c666c5d1
YeongJoon <yjk7557@naver.com>
2023-11-25 14:14:50 +0900

    [Feat] 초기 세팅

---
+++ Cookie-SwiftUI/Cookie-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookie-SwiftUI
+//
+//  Created by 김영준 on 11/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
